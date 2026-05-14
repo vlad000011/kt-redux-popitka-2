7778e49b115c1faaca22f13eeb1b4ef81a278290
vlad000011 <105325266+vlad000011@users.noreply.github.com>
2026-05-14 17:51:20 +0300

    Update teachersSlice.js

--- scr/teachers/teachersSlice.js
+++ scr/teachers/teachersSlice.js
@@ -1,8 +1,11 @@
-import {
-  createSlice,
-  createAsyncThunk,
-} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { client } from '../api/client'
+
+const initialState = {
+  items: [],
+  status: 'idle',
+}
+
 export const fetchTeachers = createAsyncThunk(
   'teachers/fetchTeachers',
   async () => {
@@ -10,26 +13,21 @@ export const fetchTeachers = createAsyncThunk(
     return response.data
   }
 )
+
 const teachersSlice = createSlice({
   name: 'teachers',
-  initialState: [],
+  initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(
-      fetchTeachers.fulfilled,
-      (state, action) => {
-        return action.payload
-      }
-    )
+    builder.addCase(fetchTeachers.fulfilled, (state, action) => {
+      state.items = action.payload
+      state.status = 'succeeded'
+    })
   },
 })
-export const selectAllTeachers = state =>
-  state.teachers
-export const selectTeacherById = (
-  state,
-  teacherId
-) =>
-  state.teachers.find(
-    teacher => teacher.id === teacherId
-  )
+
+export const selectAllTeachers = (state) => state.teachers.items
+export const selectTeacherById = (state, teacherId) =>
+  state.teachers.items.find(t => t.id === teacherId)
+
 export default teachersSlice.reducer
